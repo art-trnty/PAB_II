0079b4e6dc3bb654741cdeb61264623308f0c111
art-trnty <164327639+art-trnty@users.noreply.github.com>
2025-04-22 21:11:51 +0700

    Update Files

--- fasum/lib/screens/AddPost.dart
+++ fasum/lib/screens/AddPost.dart
@@ -11,7 +11,6 @@ import 'package:shimmer/shimmer.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
-
   @override
   State<AddPostScreen> createState() => _AddPostScreenState();
 }
@@ -23,28 +22,10 @@ class _AddPostScreenState extends State<AddPostScreen> {
   final ImagePicker _picker = ImagePicker();
   bool _isUploading = false;
   double? _latitude;
-  double? _longtitude;
+  double? _longitude;
   String? _aiCategory;
   String? _aiDescription;
   bool _isGenerating = false;
-
-  Future<void> _compressAndEncodeImage() async {
-    if (_image == null) return;
-
-    try {
-    final CompressedImage = await FlutterImageCompress.compressWithFile(
-    _image!.path,
-    quality: 50,
-    );
-
-    if (CompressedImage == null) return;
-
-    setState((){
-    _base64Image = base64Encode(CompressedImage);
-    });
-    }catch(e){}
-  }
-
   Future<void> _pickImage(ImageSource source) async {
     try {
       final pickedFile = await _picker.pickImage(source: source);
@@ -56,7 +37,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
           _descriptionController.clear();
         });
         await _compressAndEncodeImage();
-        // await _generateDescriptionWithAi();
+// await _generateDescriptionWithAI();
       }
     } catch (e) {
       if (mounted) {
@@ -67,8 +48,113 @@ class _AddPostScreenState extends State<AddPostScreen> {
     }
   }
 
+  Future<void> _compressAndEncodeImage() async {
+    if (_image == null) return;
+    try {
+      final compressedImage = await FlutterImageCompress.compressWithFile(
+        _image!.path,
+        quality: 50,
+      );
+      if (compressedImage == null) return;
+      setState(() {
+        _base64Image = base64Encode(compressedImage);
+      });
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Failed to compress image:$e')));
+      }
+    }
+  }
+
+  void _showImageSourceDialog() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.camera_alt),
+                title: const Text('Take a picture'),
+                onTap: () {
+                  Navigator.pop(context);
+                  _pickImage(ImageSource.camera);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Choose from gallery'),
+                onTap: () {
+                  Navigator.pop(context);
+                  _pickImage(ImageSource.gallery);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.cancel),
+                title: const Text('Cancel'),
+                onTap: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Post')),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            GestureDetector(
+              onTap: _showImageSourceDialog,
+              child: Container(
+                height: 250,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: _image != null
+                    ? ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.file(
+                          _image!,
+                          height: 250,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    : const Center(
+                        child: Icon(
+                          Icons.add_a_photo,
+                          size: 50,
+                          color: Colors.grey,
+                        ),
+                      ),
+              ),
+            ),
+            SizedBox(height: 24),
+            TextField(
+              controller: _descriptionController,
+              textCapitalization: TextCapitalization.sentences,
+              maxLines: 6,
+              decoration: const InputDecoration(
+                hintText: 'Add a brief description...',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Post')),
+          ],
+        ),
+      ),
+    );
   }
 }
